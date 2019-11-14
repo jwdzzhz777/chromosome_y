01d592ee2363ae3fc409c72e4fa90bbccf0948d8
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-11-14 16:10:11 +0800

    feat:add global error class

---
+++ app/lib/global/errors.ts
@@ -0,0 +1,34 @@
+interface Errors {
+    readonly status?: number;
+    readonly data?: any;
+    readonly name?: string;
+}
+
+class BaseError extends Error implements Errors {
+    data: any;
+
+    constructor(message: string, data?: any) {
+        super(message);
+        data && (this.data = data);
+    }
+}
+
+global.BadRequest = class BadRequest extends BaseError {
+    public status = 400;
+    public name = 'bad request';
+};
+
+global.InternalServerError = class InternalServerError extends BaseError {
+    public status = 500;
+    public name = 'internal server error';
+};
+
+global.UnprocessableEntityError = class UnprocessableEntityError extends BaseError {
+    public status = 422;
+    public name = 'unprocessable entity';
+};
+
+global.GitlabForbiddenError = class GitlabForbiddenError extends BaseError {
+    public status = 403;
+    public name = 'gitlab Forbidden';
+};
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,30 @@ import 'egg';
 declare module 'egg' {
 
 }
+
+declare global {
+    export class BadRequest {
+        constructor(message: string, data?: any);
+    }
+
+    export class InternalServerError {
+        constructor(message: string, data?: any);
+    }
+
+    export class UnprocessableEntityError {
+        constructor(message: string, data?: any);
+    }
+
+    export class GitlabForbiddenError {
+        constructor(message: string, data?: any);
+    }
+
+    namespace NodeJS {
+        interface Global {
+            BadRequest: BadRequest;
+            InternalServerError: InternalServerError;
+            UnprocessableEntityError: UnprocessableEntityError;
+            GitlabForbiddenError: GitlabForbiddenError;
+        }
+    }
+}
