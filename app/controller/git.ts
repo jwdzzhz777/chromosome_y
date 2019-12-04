5db3ceabb3afebb2083bab95c1594fc97d7f4426
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-04 17:34:37 +0800

    feat:方法移动

--- app/controller/git.ts
+++ app/controller/git.ts
@@ -12,20 +12,4 @@ export default class GitController extends Controller {
             message: 'success'
         });
     }
-    @path('/api/test/associate')
-    @query({
-        fileName: 'string',
-        number: 'numberString'
-    })
-    @desc('手动关联已有项目')
-    public async associate() {
-        let { query } = this.ctx;
-        let { fileName, number } = this.ctx.getAndValidate(this.getMetadata('associate').query, query);
-
-        await this.service.git.associate(fileName, +number);
-
-        this.ctx.success({
-            message: '关联成功'
-        });
-    }
 }
