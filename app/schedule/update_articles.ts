9312275e3f385b2694ada6a66424e3167cbc1c96
jwdzzhz777 <jwdzzhz777@gmail.com>
2020-01-09 17:02:33 +0800

    feat:更新定时器语法

--- app/schedule/update_articles.ts
+++ app/schedule/update_articles.ts
@@ -3,13 +3,14 @@ import { Subscription, Context } from 'egg';
 export default class UpdateArticles extends Subscription {
     static get schedule() {
         return {
-            cron: '0 0 24 * * *', // 每天 24 点准点执行
+            cron: '0 0 0 * * *', // 每天 24 点准点执行
             type: 'all',
             immediate: true // 立即执行
         };
     }
 
     async subscribe(ctx: Context) {
+        ctx.logger.info('更新 github issue 定时器');
         await ctx.service.article.autoBot();
     }
 }
